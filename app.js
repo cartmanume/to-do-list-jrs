6a18248b4cebe2cc30a3ea2c52c5b5eedb4a3053
Erik Lindskog <cartman.sweden@gmail.com>
2016-04-05 12:55:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-
+//comment
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
